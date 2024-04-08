a160db51712a88cdd0ac9c7c9fe2485dcba96149
Callum Groeger <me@callumgroeger.com>
2024-04-08 21:50:11 +0100

    make changes to main, now using to 3-gram for further research

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,37 @@ fn main() {
     let file_path = &args[1];
 
     // Create an instance of the BigramModel
-    let mut bmodel = BigramModel::new(2);
+    let mut bmodel = BigramModel::new(3);
 
     // File text_sample.txt must exist in the current path
     if let Ok(lines) = read_lines(file_path) {
         // Use lines from the iterator
         for line in lines.flatten() {
             if !line.is_empty() {
-                bmodel.update_bigram_model(line);
+                bmodel.update_ngram_model(line);
             }
         }
     }
 
-    let most_common_bigram = bmodel.most_common_bigram_without_sentence_tokens();
+    // TODO: Why can't I use both of these back to back?
 
-    assert!(most_common_bigram.is_ok());
-    println!("Most Frequent Bigram: {:?}. It occurred {:?} times.", 
-        most_common_bigram.unwrap().0,
-        most_common_bigram.unwrap().1
-    );
+    // let most_common_bigram = bmodel.most_common_ngram_without_sentence_tokens();
+    
+    // assert!(most_common_bigram.is_ok());
+    // println!("Most Frequent ngram: {:?}. It occurred {:?} times.", 
+    //     most_common_bigram.unwrap().0,
+    //     most_common_bigram.unwrap().1
+    // );
+
+    let most_common_bigram = vec![
+        "the".to_string(),
+        "United".to_string(),
+        "States".to_string()
+    ];
+    let most_common_probability = bmodel.calculate_ngram_probability(&most_common_bigram);
+    println!("The ngram probability was: {:.3}.",
+        most_common_probability
+    )
 }
 
 
